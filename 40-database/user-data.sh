106d987763368cb9838e88ec36de2024c7c3ea48
amarenugu <ammi.mailme@gmail.com>
2026-09-10 13:12:54 -0700

    roboshop

--- 40-database/user-data.sh
+++ 40-database/user-data.sh
@@ -2,4 +2,5 @@
 chattr -i /etc/ssh/sshd_config 2>/dev/null
 sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sed -i '/^PasswordAuthentication/a KbdInteractiveAuthentication no' /etc/ssh/sshd_config
+echo "ec2-user:DevOps-321" | chpasswd
 systemctl restart sshd
