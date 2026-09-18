96cc0c42c55bef8e311600a8c722b62995f0a28a
amarenugu <ammi.mailme@gmail.com>
2026-09-17 21:10:23 -0700

    roboshop

--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -1,7 +1,7 @@
 locals {
     backend_alb_sg_id   = data.aws_ssm_parameter.backend_alb_sg_id.value
     common_name         = "${var.project}-${var.environment}"
-    private_subnet_ids   = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0] # roboshop-dev-private_subnet_id-1a
+    private_subnet_ids   = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
     common_tags = {
         Project         = "${var.project}"
         Environment     = "${var.environment}"
