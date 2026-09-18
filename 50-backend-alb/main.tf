21dbdc1d6b95096a78027a9b17db02f24cd45983
amarenugu <ammi.mailme@gmail.com>
2026-09-17 20:55:52 -0700

    roboshop

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -3,13 +3,13 @@ resource "aws_lb" "backend_alb" {
   internal           = true
   load_balancer_type = "application"
   security_groups    = [local.backend_alb_sg_id]
-  subnets            = [local.private_subnet_id]
+  subnets            = [local.private_subnet_ids]
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   tags = merge (
     {
-        Name = "${local.coommon_name}-backend-alb"
+        Name = "${local.common_name}-backend-alb"
     },
     local.common_tags
   )
@@ -17,7 +17,7 @@ resource "aws_lb" "backend_alb" {
 
 
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_alb.backend.arn
+  load_balancer_arn = aws_lb.backend_alb.arn
   port              = "80"
   protocol          = "HTTP"
 
@@ -35,7 +35,7 @@ resource "aws_lb_listener" "http" {
 
 resource "aws_route53_record" "www" {
   zone_id = var.zone_id
-  name    = "*.backend-alb-${var.environment}.{var.domain_name}" # *.backend-alb-dev.ammienugu.online
+  name    = "*.backend-alb-${var.environment}.ammienugu.online" # *.backend-alb-dev.ammienugu.online
   type    = "A"
 
   alias {
